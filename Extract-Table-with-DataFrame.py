0dfbe2633eb98e8be0b4fb5a3b49aa4056f96570
gauravsekhri <59785364+gauravsekhri@users.noreply.github.com>
2020-04-13 20:14:41 +0530

    Update Extract-Table-with-DataFrame.py

--- Extract-Table-with-DataFrame.py
+++ Extract-Table-with-DataFrame.py
@@ -1,10 +1,14 @@
+#Import Liberaries
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
+#Connect to the website
 res = requests.get("https://www.dcrustedp.in/show_chart.php")
 soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0]
 df = pd.read_html(str(table))
+
+#Print the table
 print( tabulate(df[0], headers='keys', tablefmt='psql') )
